a9acd1d3b3cebac93a04f2e4edd19271424b87d1
pmocz <philip.mocz@gmail.com>
2017-11-01 07:28:12 -0400

    added example of source term

--- smoluchowski.py
+++ smoluchowski.py
@@ -47,7 +47,8 @@ def main():
   f0 = np.exp(-x)
   #f0 = x**-2
   K_A = '1'
-  f = smolsolve(x, xBound, f0, t, K_A, Nt)
+  source = 'none'
+  f = smolsolve(x, xBound, f0, t, K_A, source, Nt)
   fig = plt.plot(x, f, 'o', color=[0., 1., 0.])
 
   plt.legend(loc='lower left')
@@ -70,7 +71,7 @@ def main():
     if t > 0:
       f = np.exp(-T*x)/x**2/np.sqrt(t) * sps.iv(1,2*x*np.sqrt(t))
     else:
-	  f = np.exp(-x)/x
+      f = np.exp(-x)/x
     g = x*f
     fig = plt.plot(x, f, linewidth=2, color=[cc/5., 0., 0.], label='$t='+"%0.1f" % t +'$')
   plt.yscale('log')
@@ -84,7 +85,8 @@ def main():
   x = np.linspace(xBound[0]+0.5*dx,xBound[-1]-0.5*dx,Nx-1)
   f0 = np.exp(-x)/x
   K_A = 'x*y'
-  f = smolsolve(x, xBound, f0, t, K_A, Nt)
+  source = 'none'
+  f = smolsolve(x, xBound, f0, t, K_A, source, Nt)
   fig = plt.plot(x, f, 'o', color=[0., 1., 0.])
 
   plt.legend(loc='lower left')
@@ -102,12 +104,13 @@ def main():
   t = 3.
   Nt = 30
   Nx = 100
-  xBound = np.linspace(0.01,100,Nx)
+  xBound = np.linspace(0.01,30,Nx)
   dx = xBound[1] - xBound[0]
   x = np.linspace(xBound[0]+0.5*dx,xBound[-1]-0.5*dx,Nx-1)
   f0 = np.exp(-x)
   K_A = '(x*y)^(15/14)*(x+y)^(9/14)'
-  f = smolsolve(x, xBound, f0, t, K_A, Nt)
+  source = 'myGaussian'
+  f = smolsolve(x, xBound, f0, t, K_A, source, Nt)
   fig = plt.plot(x, f0, linewidth=2, color=[0., 0., 1.])
   fig = plt.plot(x, f, 'o', color=[0., 1., 0.])
   plt.yscale('log')
@@ -119,7 +122,7 @@ def main():
   plt.close()  
    
    
-def smolsolve(x, xBound, f0, t, K_A, Nt):
+def smolsolve(x, xBound, f0, t, K_A, source, Nt):
   """ solve Smoluchowski equations
   Input: x, initial condition, time, kernel, # timestep
   Output: solution f(t,x)
@@ -130,44 +133,54 @@ def smolsolve(x, xBound, f0, t, K_A, Nt):
   g = x * f0
   for t in range(Nt):
     JL = 0*x
+    fsrc = 0*x
+    # source term for f
+    if source == 'none':
+      fsrc = 0*x
+    elif source == 'myGaussian':
+      fsrc = 0.05*np.exp(-((x-8.)**2))		
+    else:
+      sys.exit("source incorrectly specified") 
+    
+    # Flux term
     for i in range(1,Nx):
-        for p in range(0,i):
-            # K_A = 1
-            # this is analytic expression for int_{x_j}^{x_j+1} K_A(x_mid(i),y)/y \, dy
-            if K_A == '1':
-				kernBndry = np.log(xBound[i-p]/x[i-p-1])
-				kern = np.log(xBound[i-p+1:-1]/xBound[i-p:-2])
-            elif K_A == 'x*y':
-				xA = x[i-p-1]
-				xB = xBound[i-p]
-				kernBndry = (xB - xA) * x[p]
-				xA = xBound[i-p:-2]
-				xB = xBound[i-p+1:-1]
-				kern      = (xB - xA) * x[p]
-            elif K_A == '2+(x/y)^2+(y/x)^2':
-				xA = x[i-p-1]
-				xB = xBound[i-p]
-				kernBndry = (-xA**2 + xB**2 + x[p]**4 * (1./xA**2-1./xB**2)) / (2.*x[p]**2) + 2.*np.log(xB/xA)
-				xA = xBound[i-p:-2]
-				xB = xBound[i-p+1:-1]
-				kern      = (-xA**2 + xB**2 + x[p]**4 * (1./xA**2-1./xB**2)) / (2.*x[p]**2) + 2.*np.log(xB/xA)
-            elif K_A == '(x*y)^(15/14)*(x+y)^(9/14)':  # https://arxiv.org/pdf/astro-ph/0201102.pdf
-				normConst = 0.01   # make physically meaningful!
-				xA = x[i-p-1]
-				xB = xBound[i-p]
-				kernBndry = normConst*-(7./120.) * x[p]**(15./14.) * (x[p] * (9. * x[p] * (xA/(xA+x[p])**5.)**(1./14.)+19. * (xA**15./(xA+x[p])**5.)**(1./14.)-9. * x[p] * (xB/(xB+x[p])**5.)**(1./14.)-19.*(xB**15./(xB+x[p])**5.)**(1./14.))+10.*((xA**29./(xA+x[p])**5.)**(1./14.)-(xB**29./(xB+x[p])**5.)**(1./14.))-9.*(xA * x[p]**23.)**(1./14.) * sps.hyp2f1(1./14.,5./14.,15./14.,-(xA/x[p]))+9.*(xB * x[p]**23.)**(1./14.)*sps.hyp2f1(1./14.,5./14.,15./14.,-(xB/x[p])))		
-				xA = xBound[i-p:-2]
-				xB = xBound[i-p+1:-1]
-				kern      = normConst*-(7./120.) * x[p]**(15./14.) * (x[p] * (9. * x[p] * (xA/(xA+x[p])**5.)**(1./14.)+19. * (xA**15./(xA+x[p])**5.)**(1./14.)-9. * x[p] * (xB/(xB+x[p])**5.)**(1./14.)-19.*(xB**15./(xB+x[p])**5.)**(1./14.))+10.*((xA**29./(xA+x[p])**5.)**(1./14.)-(xB**29./(xB+x[p])**5.)**(1./14.))-9.*(xA * x[p]**23.)**(1./14.) * sps.hyp2f1(1./14.,5./14.,15./14.,-(xA/x[p]))+9.*(xB * x[p]**23.)**(1./14.)*sps.hyp2f1(1./14.,5./14.,15./14.,-(xB/x[p])))
-
-            else:
-				sys.exit("kernel incorrectly specified") 
+      for p in range(0,i):
+        # K_A = 1
+        # this is analytic expression for int_{x_j}^{x_j+1} K_A(x_mid(i),y)/y \, dy
+        if K_A == '1':
+          kernBndry = np.log(xBound[i-p]/x[i-p-1])
+          kern = np.log(xBound[i-p+1:-1]/xBound[i-p:-2])
+        # K_A = x*y
+        elif K_A == 'x*y':
+          xA = x[i-p-1]
+          xB = xBound[i-p]
+          kernBndry = (xB - xA) * x[p]
+          xA = xBound[i-p:-2]
+          xB = xBound[i-p+1:-1]
+          kern      = (xB - xA) * x[p]
+        elif K_A == '2+(x/y)^2+(y/x)^2':
+          xA = x[i-p-1]
+          xB = xBound[i-p]
+          kernBndry = (-xA**2 + xB**2 + x[p]**4 * (1./xA**2-1./xB**2)) / (2.*x[p]**2) + 2.*np.log(xB/xA)
+          xA = xBound[i-p:-2]
+          xB = xBound[i-p+1:-1]
+          kern      = (-xA**2 + xB**2 + x[p]**4 * (1./xA**2-1./xB**2)) / (2.*x[p]**2) + 2.*np.log(xB/xA)
+        elif K_A == '(x*y)^(15/14)*(x+y)^(9/14)':  # https://arxiv.org/pdf/astro-ph/0201102.pdf
+          normConst = 0.001   # make physically meaningful!
+          xA = x[i-p-1]
+          xB = xBound[i-p]
+          kernBndry = normConst*-(7./120.) * x[p]**(15./14.) * (x[p] * (9. * x[p] * (xA/(xA+x[p])**5.)**(1./14.)+19. * (xA**15./(xA+x[p])**5.)**(1./14.)-9. * x[p] * (xB/(xB+x[p])**5.)**(1./14.)-19.*(xB**15./(xB+x[p])**5.)**(1./14.))+10.*((xA**29./(xA+x[p])**5.)**(1./14.)-(xB**29./(xB+x[p])**5.)**(1./14.))-9.*(xA * x[p]**23.)**(1./14.) * sps.hyp2f1(1./14.,5./14.,15./14.,-(xA/x[p]))+9.*(xB * x[p]**23.)**(1./14.)*sps.hyp2f1(1./14.,5./14.,15./14.,-(xB/x[p])))		
+          xA = xBound[i-p:-2]
+          xB = xBound[i-p+1:-1]
+          kern      = normConst*-(7./120.) * x[p]**(15./14.) * (x[p] * (9. * x[p] * (xA/(xA+x[p])**5.)**(1./14.)+19. * (xA**15./(xA+x[p])**5.)**(1./14.)-9. * x[p] * (xB/(xB+x[p])**5.)**(1./14.)-19.*(xB**15./(xB+x[p])**5.)**(1./14.))+10.*((xA**29./(xA+x[p])**5.)**(1./14.)-(xB**29./(xB+x[p])**5.)**(1./14.))-9.*(xA * x[p]**23.)**(1./14.) * sps.hyp2f1(1./14.,5./14.,15./14.,-(xA/x[p]))+9.*(xB * x[p]**23.)**(1./14.)*sps.hyp2f1(1./14.,5./14.,15./14.,-(xB/x[p])))
+        else:
+          sys.exit("kernel incorrectly specified") 
             
-            JL[i] = JL[i] + dx*g[p] * (kernBndry*g[i-p-1] + np.sum(kern*g[i-p:-1]))
+        JL[i] = JL[i] + dx*g[p] * (kernBndry*g[i-p-1] + np.sum(kern*g[i-p:-1]))
     
     JR = np.roll(JL,-1)
     JR[-1]= 0
-    g = g - dt / dx * ( JR - JL )
+    g = g - dt / dx * ( JR - JL ) + dt*fsrc*x
 
   f = g / x
   return f
